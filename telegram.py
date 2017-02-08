ec90975baff1e820ef1bd93d7b52ecfd9168ae20
wndenis <winda.wzse@gmail.com>
2017-02-09 00:16:36 +0300

    Добавлен вывод сообщения ошибки

--- telegram.py
+++ telegram.py
@@ -1,14 +1,14 @@
-import config
 import telebot
 
+import config
 from logger import plog
 
 
 def sendTeleMsg(msg):
-    bot = telebot.TeleBot(config.telegram_api_token)
     channel = "@isbo0116"
     try:
+        bot = telebot.TeleBot(config.telegram_api_token)
         bot.send_message(channel, msg)
         plog("[TLGRM] В телеграм тоже скинула")
-    except:
-        plog("[TLGRM] В телеграм не скинула")
+    except Exception as e:
+        plog("[TLGRM] Не смогла отправить в телеграм: %s" % e)
