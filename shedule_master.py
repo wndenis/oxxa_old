2301e96db7dc9ba2c0f1caac091f9c47302539f4
wndenis <winda.wzse@gmail.com>
2017-02-09 00:15:41 +0300

    Отлов ошибок при загрузке расписания Новый алгоритм выборки расписаная для нового формата (2017) - выделение времени, названия, типа работы, аудитории Предварительный отчет с "сырым" расписанием в личку TODO: Отлавливать диапазоны недель вида "1,3,5 н", "кр 3, 5, 7 н"

--- shedule_master.py
+++ shedule_master.py
@@ -6,19 +6,25 @@ import re
 import xlrd
 from grabber import getWeekNum, getWeather, getShedUrl, getLastModifiedS
 from logger import plog
-from uti import getCacheProp, jDump, setCacheProp, getLastModified
+from uti import getCacheProp, jDump, setCacheProp, getLastModified, check_and_create_dir
 from vk import sendMsg
 
 
 def downloadShed(url):
+
     try:
         st_shed = getCacheProp('st_shed')
     except:
         plog("[DownloadShed] ошибка в st_shed")
         st_shed = -1
+
     dest = "data\\shedules\\xls\\%s.xls" % st_shed
     plog("[DownloadShed] Скачиваю %s" % url)
-    urlretrieve(url, dest)
+    try:
+        urlretrieve(url, dest)
+    except Exception as e:
+        plog("[DownloadShed] Я не смогла скачать расписание: %s" % e)
+        return
     plog("[DownloadShed] Расписание скачано!")
     plog("%s.xls\n" % st_shed)
     parse_file_to_json("%s" % st_shed)
@@ -26,7 +32,9 @@ def downloadShed(url):
 
 def parse_file_to_json(file):
     plog('[ParseFileToJson] Начинаю парсинг...')
-    shed = xlrd.open_workbook("data\\shedules\\xls\\%s.xls" % (file), formatting_info=True)
+
+    shed = xlrd.open_workbook("data\\shedules\\xls\\%s.xls" % file)  # , formatting_info=True)
+
     sheet = shed.sheet_by_index(0)
     jsonShedules = dict()
     for group in ["ИСБО-01-16"]:
@@ -38,68 +46,61 @@ def parse_file_to_json(file):
             for c_el in row:
                 x += 1
                 if str(c_el).rstrip() == group:
-                    lessonData = sheet.col_values(x, start_rowx=y + 2, end_rowx=y + 6 * 6)
-                    auditoryData = sheet.col_values(x + 1, start_rowx=y + 2, end_rowx=y + 6 * 6)
-
+                    timeStartData = sheet.col_values(x + 5, start_rowx=y + 2, end_rowx=y + 2 + 12 * 6)
+                    timeEndData = sheet.col_values(x + 6, start_rowx=y + 2, end_rowx=y + 2 + 12 * 6)
+                    lessonData = sheet.col_values(x, start_rowx=y + 2, end_rowx=y + 2 + 12 * 6)
+                    workData = sheet.col_values(x + 1, start_rowx=y + 2, end_rowx=y + 2 + 12 * 6)
+                    workData = [x.upper() for x in workData]
+                    auditoryData = sheet.col_values(x + 2, start_rowx=y + 2, end_rowx=y + 2 + 12 * 6)
+                    summary = [(lessonData[i], workData[i], auditoryData[i]) for i in range(12*6)]
                     parsedData = [
-                        ([lesson, auditory
-                          ]) if lesson != "" else ["НЕТ ПАРЫ", ""] for lesson, auditory in
-                        zip(lessonData, auditoryData)]
-                    packedData = [parsedData[i * 6:i * 6 + 6] for i in range(len(parsedData) // 5)]
+                        ([start, end, lesson, work, auditory
+                          ]) if lesson != "" else [start, end, "НЕТ ПАРЫ", "", ""] for start, end, lesson, work, auditory in
+                        zip(timeStartData, timeEndData, lessonData, workData, auditoryData)]
 
-        completeShed = []  # Полное расписание. isbo:[{[пара, cond:{1,2,3}]}]
-        IDays = [x for x in range(30)[1::2]]  # Нечетные
-        IIDays = [x for x in range(30)[2::2]]  # Четные
+                    report = "Вот что я там нашла:\n"
+                    for elem in parsedData:
+                        report += str(elem) + "\n"
+                    sendMsg("30903046", report)
+
+                    # пакуем пары 1/2
+                    packedData = [parsedData[i * 2:i * 2 + 2] for i in range(len(parsedData) // 2)]
+                    # пакуем дни 12 12 12 12 12 12
+                    packedData = [packedData[i * 6:i * 6 + 6] for i in range(len(packedData) // 6)]
+
         jsonWeek = []
         for day in packedData:
             jsonDay = {}  # Разобранное расписание ДНЯ
-            lessonNum = 0
-            for lesson in day:
-                lessonNum += 1
-                auditories = re.sub(r" ", r"", lesson[1]).split("\n")
-                lesson[0] = " " + lesson[0]
+            pairNum = 0
+            for pair in day:
+                jsonPair = {}
+                pairNum += 1
+                for i in (0, 1):
+                    lesson = pair[i]
+                    lessonName = lesson[2]
 
-                # I
-                try:
-                    resI = re.search(r"[^I]I н.", lesson[0])
-                except:
-                    resI = None
-                # II
-                try:
-                    resII = re.search(r"II н.", lesson[0])
-                except:
-                    resII = None
-                    # d-d
-                try:
-                    resRng = re.search(r"\d+-\d+ н.", lesson[0])
-                except:
-                    resRng = None
+                    lessonWork = ""
+                    if lesson[3] != "":
+                        lessonWork = lesson[3]
 
-                if resRng is not None:  # Вытаскиваем диапазон недель
-                    rngA, rngB = [int(x) for x in lesson[0][resRng.start():resRng.end() - 3].split('-')]
-                    cond = [x for x in range(rngA, rngB+1)]
-                    jsonLesson = [{"lesson": lesson[0][resRng.end() + 1:], "auditory": lesson[1], "cond": cond}]
-                    # plog(jsonDay)
+                    lessonAuditory = ""
+                    if lesson[4] != "":
+                        lessonAuditory = "(" + lesson[4] + ")"
 
-                elif resI is not None and resII is None:  # одна пара по нч
-                    jsonLesson = [{"lesson": lesson[0][resI.end() + 1:], "auditory": lesson[1], "cond": IDays}]
+                    jsonLesson = {"lesson": "%s %s %s" % (lessonName, lessonWork, lessonAuditory)}
 
-                elif resI is None and resII is not None:  # одна пара по ч
-                    jsonLesson = [{"lesson": lesson[0][resII.end() + 1:], "auditory": lesson[1], "cond": IIDays}]
-
-                elif resI is not None and resII is not None:  # пара по ч и пара по нч
-                    lessonI = lesson[0][resI.end():resII.start()]
-                    lessonII = lesson[0][resII.end():]
-                    auditoryI, auditoryII = lesson[1].split("\n")
-                    jsonLesson = [{"lesson": lessonI, "auditory": auditoryI, "cond": IDays},
-                                  {"lesson": lessonII, "auditory": auditoryII, "cond": IIDays}]
-
-                elif (resI == None and resII == None):
-                    cond = IDays + IIDays
-                    jsonLesson = [{"lesson": lesson[0], "auditory": lesson[1], "cond": cond}]
-                jsonDay[lessonNum] = jsonLesson
+                    if i % 2 == 0:  # нечетный
+                        pairTimeStart = re.sub('-', ':', lesson[0])
+                        pairTimeEnd = re.sub('-', ':', lesson[1])
+                        jsonPair["odd"] = jsonLesson
+                        jsonPair["time"] = "[%s-%s]" % (pairTimeStart, pairTimeEnd)
+                    else:  # четный
+                        jsonPair["even"] = jsonLesson
+                    jsonDay[pairNum] = jsonPair
+                jsonDay[pairNum] = jsonPair
             jsonWeek.append(jsonDay)
         jsonShedules[group] = jsonWeek
+
     jDump("data\\shedules\\json\\%s.json" % file, jsonShedules)
     plog('[ParseFileToJson] Парсинг завершен.')
     plog('====================')
@@ -108,43 +109,26 @@ def parse_file_to_json(file):
 def getDailyShed(day, week, group):
     with open("data\\shedules\\json\\%s.json" % getCacheProp('st_shed'), mode='r', encoding='utf-8') as f:
         json_data = json.load(f)
-
     dayData = json_data[group][day]
-    shedule = []
 
-    weekNum = week
-    lessonCount = 0
-    for lessonNum in sorted(dayData):
-        for option in dayData[lessonNum]:
-            if option['lesson'] != 'EMPTY':
-                if weekNum in option['cond']:
-                    if option['auditory'] != '':
-                        auditory = "(%s)" % option['auditory']
-                    else:
-                        auditory = ''
-                    shedule.append("%s %s" % (option['lesson'].strip().split("\n")[0], auditory))
-                    break
-            if dayData[lessonNum][-1] == option:
-                shedule.append("ОКНО")
-    lStart = ''
-    lEnd = ''
-    for lesson in reversed(shedule):  # Очищаем последние пустые пары
-        if lesson.strip() in ['ОКНО', 'НЕТ ПАРЫ']:
-            shedule.pop()
-        else:
-            lEnd = len(shedule) - 1
-            break
-    # Начало пар
-    i = 0
-    for lesson in shedule:
-        if not lesson in ['ОКНО', 'НЕТ ПАРЫ']:
-            break
-        i += 1
-    startT = ['9:00', '10:45', '12:50', '14:35', '16:20', '18:00', '19:45']
-    endT = ['10:35', '12:20', '14:25', '16:10', '17:55', '19:35', '21:20']
+    if week % 2 == 0:
+        weekType = "even"
+    else:
+        weekType = "odd"
+
     ans = ""
-    for x in enumerate(shedule):
-        ans += ("%s) [%s-%s]\n%s\n" % (x[0] + 1, startT[x[0]], endT[x[0]], x[1]))
+    pairNum = 0
+    shed = [dayData[pair] for pair in sorted(dayData)]
+    # Если пара пустая, то не добавлять ее
+    for pair in reversed(shed):
+        if "НЕТ ПАРЫ" in pair[weekType]["lesson"]:
+            shed.pop()
+        else:
+            break
+
+    for pair in shed:
+        pairNum += 1
+        ans += "%s) %s\n%s\n" % (pairNum, pair["time"], pair[weekType]["lesson"])
     return ans
 
 
@@ -155,7 +139,7 @@ def compileShed(group):
     day = datetime.weekday(dt)
     if day != 5:
         plog('[CompileShed] Day != 5')
-        week = getWeekNum();
+        week = getWeekNum()
         if day == 6 or day == 0:
             day = 0
             week += 1
